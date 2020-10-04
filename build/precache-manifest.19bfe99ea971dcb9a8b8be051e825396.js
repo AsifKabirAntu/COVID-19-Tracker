1deb7422d19efa09acc59ee5b5747b67d4d0f171
Asif_Vcube <asif_vcube@Asif-Vcubes-MacBook-Air.local>
2020-10-04 09:00:06 +0600

    initial commit

---
+++ build/precache-manifest.19bfe99ea971dcb9a8b8be051e825396.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c05830c2dab362aff57e71097cb398",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b6518c330ddce613687",
+    "url": "/static/css/2.067ccc96.chunk.css"
+  },
+  {
+    "revision": "eced5519649fbe74fdc7",
+    "url": "/static/css/main.cd3a87a6.chunk.css"
+  },
+  {
+    "revision": "2b6518c330ddce613687",
+    "url": "/static/js/2.c566785b.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/static/js/2.c566785b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eced5519649fbe74fdc7",
+    "url": "/static/js/main.47f7892a.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
